ce1fa552344872c61c0ef6da3fe23dd84b9d53f8
tpqls774 <tpqls774@dgsw.hs.kr>
2024-08-08 16:49:15 +0900

    Main
    메인 페이지 완성

--- JS/main.js
+++ JS/main.js
@@ -13,32 +13,71 @@ window.addEventListener('scroll', function () {
     })
 });
 
-
 const moveButtons = document.querySelectorAll(".arrow");
 const imgBox = document.querySelector(".img-box");
 const slideImgs = document.querySelectorAll(".slide-img");
+const dots = document.querySelectorAll(".dots div");
 
 let currentIndex = 0;
 const totalSlides = slideImgs.length;
+const imageWidth = slideImgs[0].clientWidth + 120;
 
-moveButtons.forEach(button => button.addEventListener("click", slideImage));
+function updateDots() {
+    dots.forEach((dot, idx) => {
+        if (idx === currentIndex % 3) {
+            dot.classList.add("active");
+        } else if (idx === -(currentIndex % 3)) {
+            dot.classList.add("active");
+        } else {
+            dot.classList.remove("active");
+        }
+    });
+}
 
-function moveImage(direction) {
-    if (direction === 'next') {
-        currentIndex = (currentIndex + 1) % totalSlides;
-    } else {
-        currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-    }
+function updateFocus() {
+    slideImgs.forEach((img, idx) => {
+        if (idx === (currentIndex % 3) + 4) {
+            img.classList.add("focus");
+        } else {
+            img.classList.remove("focus");
+        }
+    });
+}
 
-    const offset = -currentIndex * 565;
+function moveImage() {
     imgBox.style.transition = `transform 1s ease`;
-    imgBox.style.transform = `translateX(${offset}px)`;
+    imgBox.style.transform = `translateX(${-currentIndex * imageWidth}px)`;
 }
 
-function slideImage(event) {
+moveButtons.forEach(button => button.addEventListener("click", (event) => {
     if (event.target.classList.contains('next')) {
-        moveImage('next');
+        currentIndex++;
+        moveImage();
+        updateFocus();
+        if (currentIndex === totalSlides - 6) {
+            setTimeout(() => {
+                imgBox.style.transition = 'none';
+                currentIndex = 0;
+                imgBox.style.transform = `translateX(${-currentIndex * imageWidth}px)`;
+                updateFocus();
+            }, 1000);
+        }
     } else if (event.target.classList.contains('prev')) {
-        moveImage('prev');
+        currentIndex--;
+        moveImage();
+        updateFocus();
+        if (currentIndex === -3) {
+            setTimeout(() => {
+                imgBox.style.transition = 'none';
+                currentIndex = 0;
+                imgBox.style.transform = `translateX(${-currentIndex * imageWidth}px)`;
+                updateFocus();
+            }, 1000);
+        }
     }
-}
+    updateDots();
+}));
+
+updateDots();
+updateFocus();
+imgBox.style.transform = `translateX(${-currentIndex * imageWidth}px)`;
