d846d93798451b94bd12d51cfb459e8879fb1d4e
tpqls774 <tpqls774@dgsw.hs.kr>
2024-08-12 10:24:56 +0900

    signup

--- js/signup.js
+++ js/signup.js
@@ -15,6 +15,8 @@ submit.addEventListener('click', () => {
         password: password
     };
 
+    submit.disabled = true;
+
     fetch('http', {
         method: 'POST',
         headers: {
@@ -23,14 +25,18 @@ submit.addEventListener('click', () => {
         body: JSON.stringify(signUpData)
     })
 
-    .then(response => response.json())
-    .then(data => {
-        console.log('성공:', data);
-        alert('성공');
-    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('성공:', data);
+            alert('성공');
+        })
 
-    .catch((error) => {
-        console.error('실패:', error);
-        alert('실패');
-    });
-})
+        .catch((error) => {
+            console.error('실패:', error);
+            alert('실패');
+        })
+        
+        .finally(() => {
+                submit.disabled = false;
+            });
+        })
